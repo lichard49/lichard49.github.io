d73a7393b5d3a80ae72d25f862f950944814cde1
Richard Li <lichard49@gmail.com>
2025-04-16 01:54:07 -0700

    Add titles to pages

--- templates/render.py
+++ templates/render.py
@@ -12,18 +12,18 @@ if __name__ == '__main__':
 
   home_template = environment.get_template('home_template.html')
   home_html = home_template.render()
-  home_html = base_template.render(content=home_html, tab='home')
+  home_html = base_template.render(content=home_html, tab='home', page_name='Home')
   with open(os.path.join('..', 'index.html'), 'w') as output_file:
     output_file.write(home_html)
 
   research_template = environment.get_template('research_template.html')
   research_html = research_template.render(papers=papers)
-  research_html = base_template.render(content=research_html, tab='research')
+  research_html = base_template.render(content=research_html, tab='research', page_name='Research')
   with open(os.path.join('..', 'research.html'), 'w') as output_file:
     output_file.write(research_html)
 
   teaching_template = environment.get_template('teaching_template.html')
   teaching_html = teaching_template.render()
-  teaching_html = base_template.render(content=teaching_html, tab='teaching')
+  teaching_html = base_template.render(content=teaching_html, tab='teaching', page_name='Teaching')
   with open(os.path.join('..', 'teaching.html'), 'w') as output_file:
     output_file.write(teaching_html)
