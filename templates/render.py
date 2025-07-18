98dc603f6be6a509473a6996cb854b7bf0298517
Richard Li <lichard49@gmail.com>
2025-07-18 09:22:33 -0700

    Added CV

--- templates/render.py
+++ templates/render.py
@@ -4,8 +4,16 @@ import os
 
 
 if __name__ == '__main__':
-  papers = pd.read_csv(os.path.join('data', 'papers.csv'), keep_default_na=False)
-  print(papers)
+  data = {
+    'employment': pd.read_csv(os.path.join('data', 'employment.csv'), keep_default_na=False),
+    'education': pd.read_csv(os.path.join('data', 'education.csv'), keep_default_na=False),
+    'awards': pd.read_csv(os.path.join('data', 'awards.csv'), keep_default_na=False),
+    'papers': pd.read_csv(os.path.join('data', 'papers.csv'), keep_default_na=False),
+    'talks': pd.read_csv(os.path.join('data', 'talks.csv'), keep_default_na=False),
+    'teaching': pd.read_csv(os.path.join('data', 'teaching.csv'), keep_default_na=False),
+    'mentorship': pd.read_csv(os.path.join('data', 'mentorship.csv'), keep_default_na=False),
+    'service': pd.read_csv(os.path.join('data', 'service.csv'), keep_default_na=False),
+  }
 
   environment = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
   base_template = environment.get_template('base_template.html')
@@ -17,7 +25,7 @@ if __name__ == '__main__':
     output_file.write(home_html)
 
   research_template = environment.get_template('research_template.html')
-  research_html = research_template.render(papers=papers)
+  research_html = research_template.render(papers=data['papers'])
   research_html = base_template.render(content=research_html, tab='research', page_name='Research')
   with open(os.path.join('research.html'), 'w') as output_file:
     output_file.write(research_html)
@@ -27,3 +35,8 @@ if __name__ == '__main__':
   teaching_html = base_template.render(content=teaching_html, tab='teaching', page_name='Teaching')
   with open(os.path.join('teaching.html'), 'w') as output_file:
     output_file.write(teaching_html)
+
+  cv_template = environment.get_template('cv_template.html')
+  cv_html = cv_template.render(**data)
+  with open(os.path.join('cv.html'), 'w') as output_file:
+    output_file.write(cv_html)
